dd127bb074b8053aa045367f1cc15de4300b86c5
Jaysidh <jay.dumbali@gmail.com>
2020-07-12 18:05:11 +0530

    Updates

---
+++ precache-manifest.4347e5c66f8adbd5921fa31db72dd178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d676dcc5d7521b2ca920e7ff0f8dd0e",
+    "url": "/whats-my-season/index.html"
+  },
+  {
+    "revision": "ef4888b26b0c83a2623c",
+    "url": "/whats-my-season/static/css/main.c0bd5fcb.chunk.css"
+  },
+  {
+    "revision": "b36e70b2bb453b7e7f46",
+    "url": "/whats-my-season/static/js/2.7e9d8717.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whats-my-season/static/js/2.7e9d8717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef4888b26b0c83a2623c",
+    "url": "/whats-my-season/static/js/main.e67f1507.chunk.js"
+  },
+  {
+    "revision": "25abf69012081be8b88f",
+    "url": "/whats-my-season/static/js/runtime-main.b418449b.js"
+  }
+]);
